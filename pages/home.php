6147403e532403623d3d9338aad7f57c2f7efd93
Pedro Kunz <56512310+pedrohkunz@users.noreply.github.com>
2022-11-18 23:37:54 -0300

    Add files via upload

---
+++ pages/home.php
@@ -0,0 +1,4 @@
+<?php 
+    require_once('../pages/navbar.php')
+?>
+<img src="../img/banner.png" alt="Banner" class="banner">
---
+++ pages/navbar.php
@@ -0,0 +1,23 @@
+<?php 
+    require_once("../pages/head.php");
+?>
+
+<body>
+    <section id="menu">
+        <head>
+            <a href="../index.php"><img id="logo" src="../img/logosite.png" alt="Logo do site"></a>
+            <div id="barradepesquisa">
+            <div id="divBusca">
+                <input type="text" id="txtBusca" placeholder="Pesquisar"/>
+                <a href="#"><img src="../img/lupa.png" id="btnBusca" alt="Buscar"/></a>
+            </div>
+            </div>
+        </head>  
+    </section>
+    <nav id="nav">
+        <a href="../index.php" id="itens1">Home</a>
+        <a href="#">Categorias </a>
+        <a href="#" id="itens2">Sobre</a>
+    </nav>
+</body>
+</html>

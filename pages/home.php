273cb587d23b6ff4c2400b12cb642adb7b2a15b1
Pedro Kunz <56512310+pedrohkunz@users.noreply.github.com>
2022-11-19 18:47:50 -0300

    Add files via upload

--- pages/home.php
+++ pages/home.php
@@ -2,3 +2,9 @@
     require_once('../pages/navbar.php')
 ?>
 <img src="../img/banner.png" alt="Banner" class="banner">
+<section id="titulo">
+    <h1>Categorias</h1>
+</section>
+<section id="linha">
+    <hr>
+</section>
